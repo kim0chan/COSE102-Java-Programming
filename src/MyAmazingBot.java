f9f227904bcc2c9f36b4d98c5104902fedc1fa41
kim0chan <1999@korea.ac.kr>
2018-11-19 14:50:22 +0900

    updating

---
+++ src/MyAmazingBot.java
@@ -0,0 +1,29 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class MyAmazingBot extends TelegramLongPollingBot {
+	@Override
+	public void onUpdateReceived(Update update) {
+		if(update.hasMessage() && update.getMessage().hasText()) {
+			SendMessage message = new SendMessage()
+					.setChatId(update.getMessage().getChatId())
+					.setText(update.getMessage().getText());
+			try {
+				execute(message);
+			} catch (TelegramApiException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public String getBotUsername() {
+		return "JansoriBot";
+	}
+	
+	public String getBotToken() {
+		return "734999045:AAG4XUexxr7ImZMwIU7wS7KTM45SGtnrKCc";
+	}
+ }
