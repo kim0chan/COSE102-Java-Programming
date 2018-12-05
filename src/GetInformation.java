a5b0858536fdd219d97506ed82f7deccd386bb3f
kim0chan <1999@korea.ac.kr>
2018-12-05 23:19:51 +0900

    테스트 일단은 완료

--- src/GetInformation.java
+++ src/GetInformation.java
@@ -7,7 +7,7 @@ import org.jsoup.select.Elements;
 
 public class GetInformation {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		String Url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&query=%EA%B0%95%EC%9B%90%20%EC%9B%90%EC%A3%BC%EC%8B%9C%20%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80";
 		Document doc;
 		try {
@@ -20,6 +20,25 @@ public class GetInformation {
 			
 		} catch (IOException e) {
 			// Connection fail
+			e.printStackTrace();
+		}
+	}*/
+	
+	public static void main(String[] args) {
+		String UrlT = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+날씨";
+		String UrlF = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+미세먼지";
+		Document doc;
+		
+		try {
+			doc = Jsoup.connect(UrlT).get();
+			int temperature = Integer.parseInt(doc.select("div.today_area._mainTabContent").select("span.todaytemp").text());
+			
+			doc = Jsoup.connect(UrlF).get();
+			int finedust = Integer.parseInt(doc.select("em.main_figure").text());
+			
+			System.out.println(temperature + " " + finedust);
+		} catch (IOException e) {
+			//Connection fail
 			e.printStackTrace();
 		}
 	}
