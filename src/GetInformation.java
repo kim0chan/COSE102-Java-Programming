efa1a44ad3c7cf22e3c1e3ec68a3d175427f9af9
kim0chan <1999@korea.ac.kr>
2018-12-05 22:58:23 +0900

    getinformation class의 전반적인 테스트 완료

--- src/GetInformation.java
+++ src/GetInformation.java
@@ -2,17 +2,22 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class GetInformation {
 
 	public static void main(String[] args) {
-		String Url = "http://www.kweather.co.kr/forecast/forecast_lifestyle_detail.html?main_map=1&area1=area8&area2=42110000_101";
+		String Url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&query=%EA%B0%95%EC%9B%90%20%EC%9B%90%EC%A3%BC%EC%8B%9C%20%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80";
 		Document doc;
 		try {
 			doc = Jsoup.connect(Url).get();
-			Elements elem = doc.select("span[id=\"day0tempMax0\"]");
-			System.out.println(elem.text());
+			int finedust = Integer.parseInt(doc.select("em.main_figure").text());
+			String temperature = doc.select("span.num").text();
+			
+			System.out.println(finedust);
+			System.out.println(temperature);
+			
 		} catch (IOException e) {
 			// Connection fail
 			e.printStackTrace();
