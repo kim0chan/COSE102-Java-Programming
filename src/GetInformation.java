03dc1f23892573a7b5f8162b098ba7b508121943
kim0chan <1999@korea.ac.kr>
2018-12-06 11:15:38 +0900

    문장생성기 작성중입니다

--- src/GetInformation.java
+++ src/GetInformation.java
@@ -6,40 +6,43 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class GetInformation {
-
-	/*public static void main(String[] args) {
-		String Url = "https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&query=%EA%B0%95%EC%9B%90%20%EC%9B%90%EC%A3%BC%EC%8B%9C%20%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80";
-		Document doc;
-		try {
-			doc = Jsoup.connect(Url).get();
-			int finedust = Integer.parseInt(doc.select("em.main_figure").text());
-			String temperature = doc.select("span.num").text();
-			
-			System.out.println(finedust);
-			System.out.println(temperature);
-			
-		} catch (IOException e) {
-			// Connection fail
-			e.printStackTrace();
-		}
-	}*/
+	int temperature;
+	int finedust;
 	
-	public static void main(String[] args) {
-		String UrlT = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+날씨";
-		String UrlF = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+미세먼지";
+	private int getTemperature() { //접근 지정자
+		String url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+날씨";
 		Document doc;
 		
 		try {
-			doc = Jsoup.connect(UrlT).get();
-			int temperature = Integer.parseInt(doc.select("div.today_area._mainTabContent").select("span.todaytemp").text());
-			
-			doc = Jsoup.connect(UrlF).get();
-			int finedust = Integer.parseInt(doc.select("em.main_figure").text());
-			
-			System.out.println(temperature + " " + finedust);
-		} catch (IOException e) {
+			doc = Jsoup.connect(url).get();
+			temperature = Integer.parseInt(doc.select("div.today_area._mainTabContent").select("span.todaytemp").text());
+		} catch(IOException e) {
 			//Connection fail
 			e.printStackTrace();
 		}
+		return temperature;
 	}
+	
+	private int getFinedust() { //접근 지정자
+		String url = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=안암동+미세먼지";
+		Document doc;
+		
+		try {
+			doc = Jsoup.connect(url).get();
+			finedust = Integer.parseInt(doc.select("em.main_figure").text());
+		} catch(IOException e) {
+			//Connection fail
+			e.printStackTrace();
+		}
+		return finedust;
+	}
+	
+	/*public String generateSentence() {
+		int tmp = getTemperature();
+		int fnd = getFinedust();
+		String tmp_o = new String();
+		String fnd_o = new String();
+		
+		
+	}*/
 }
